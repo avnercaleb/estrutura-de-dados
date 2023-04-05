89adc5966c7fc5d6d4334525390ee0061c42a3da
avnercaleb <avnercaleb@hotmail.com>
2023-04-04 21:03:50 -0300

    Criação dos metodos de busca (por elementos e por posição), sobrecarga do metodo adiciona (inserir em qualquer posição desejada) e metodo de aumentar o tamanho do vetor

--- src/vetores/App.java
+++ src/vetores/App.java
@@ -4,13 +4,14 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		var vetor = new Vetor(10);
+		var vetor = new Vetor(5);
+		vetor.adicionar(2);
+		vetor.adicionar(3);
+		vetor.adicionar(4);
 		vetor.adicionar(5);
-		vetor.adicionar(10);
-		vetor.adicionar(1);
-		vetor.adicionar(35);
-		vetor.adicionar(15);
-		System.out.println(vetor.toString());
+		vetor.adicionar(6);
+		vetor.adicionar(7);
+		
+		System.out.println(vetor);
 	}
-
 }
--- src/vetores/Vetor.java
+++ src/vetores/Vetor.java
@@ -10,17 +10,57 @@ public class Vetor {
 		this.tamanho = 0;
 	}
 	
-	public void adicionar(Integer novoElemento) {
+	public boolean adicionar(Integer novoElemento) {
+		this.aumentarCapacidade();
 		if(this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = novoElemento;
 			this.tamanho++;
+			return true;
 		} else {
-			throw new ArrayIndexOutOfBoundsException("Vetor ja esta em sua capacidade maxima");
+			return false;
 		}
+	}
+	
+	public void adicionar(int posicao, int elemento) {
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("A posicao não existe no vetor");
+		}
+		
+		this.aumentarCapacidade();
+		for(int i = tamanho - 1; i >= posicao; i--) {
+			this.elementos[i + 1] = this.elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+	}
+	
+	public String buscarPosicao(int posicao) {
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("A posicao não existe no vetor");
+		}		
+		return this.elementos[posicao].toString();
+	}
+	
+	public boolean buscarElemento(int elemento) {
+		for(int i = 0; i < tamanho; i++) {
+			if(this.elementos[i] == elemento) return true;
+		}
+		return false;
 	}
 	
 	public int tamanho() {
 		return this.tamanho;
+	}
+	
+	private void aumentarCapacidade() {
+		if(tamanho == elementos.length) {
+			Integer[] novoVetor = new Integer[elementos.length * 2];
+			
+			for(int i = 0; i < tamanho; i++) {
+				novoVetor[i] = elementos[i];
+			}
+			this.elementos = novoVetor;
+		}
 	}
 	
 	public String toString() {
